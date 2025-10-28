7e3b6a2a09994f2d9876002c3741fbb822ffeaae
Luca Tomarelli <lucatomarelli1@gmail.com>
2025-10-28 20:30:49 +0100

    style mix series index

--- src/app/mix-series/page.tsx
+++ src/app/mix-series/page.tsx
@@ -1,14 +1,18 @@
+
 import "./page.css";
 import Link from "next/link";
 import MixSeriesData from "@/lib/mixSeriesData";
-
-export default function MixSeriesPage() {
-
-    return <div>
-        {MixSeriesData.map((mixSeries, index) => (
-            <Link key={index} href={`/mix-series/${mixSeries.id}`}>
-                <h2 className="mixSeriesElement">{mixSeries.name}</h2>
-            </Link>
-        ))}
-    </div>;
+export default function MixSeries() {
+    return (
+        <div className="mixSeriesSimplePageContainer">
+            <div className="mixSeriesSimpleTitleWrap">
+                <h1 className="mixSeriesSimpleTitle">Autonoma Industriale<br /><span className="mixSeriesSimpleSubtitle">Mix Series</span></h1>
+            </div>
+            {MixSeriesData.map((mixSeries, index) => (
+                <Link key={index} href={`/mix-series/${mixSeries.id}`}>
+                    <h2 className="mixSeriesSimpleLink">{mixSeries.name}</h2>
+                </Link>
+            ))}
+        </div>
+    );
 }
