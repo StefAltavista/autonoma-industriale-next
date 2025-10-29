f3bbecee60087568d0bc8155cf2dbae543d1b795
Stef Altavista <stefanomsc@hotmail.it>
2025-10-29 14:54:15 +0100

    Player UI developed, open image modal, link on title

--- src/app/mix-series/[id]/page.tsx
+++ src/app/mix-series/[id]/page.tsx
@@ -1,9 +1,11 @@
-//client-side-rendering
+"use client";
 import MixSeriesData from "@/lib/mixSeriesData";
 import "./page.css";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import Link from "next/link";
+import { useState } from "react";
+import ModalView from "@/components/ModalView";
 
 type eventPageParams = {
   params: { id: string };
@@ -11,6 +13,7 @@ type eventPageParams = {
 
 export default function Mix({ params }: eventPageParams) {
   const mix = MixSeriesData.find((mix) => mix.id === Number(params.id));
+  const [openImage, setOpenImage] = useState(false);
   if (!mix) notFound();
   const newLineOnDot = (paragraph: string) =>
     paragraph.replace(/\. (?=[A-Z])/g, ".\n\n");
@@ -22,52 +25,56 @@ export default function Mix({ params }: eventPageParams) {
   //   : null;
 
   return (
-    <div
-      id="mixPage"
-      style={{
-        // backgroundImage: `url(${mix.coverImage})`,
-        backgroundColor: "rgba(0, 0, 0)",
-        // backgroundBlendMode: "multiply",
-      }}
-    >
-      <div className="rowContainer">
-        <div className="leftColumn">
-          <Image
-            src={mix.coverImage}
-            alt={`${mix.name} cover image`}
-            className="mixCoverImage mixCoverImage-desktop"
-            width={500}
-            height={500}
-          />
-          <Link
-            href="/mix-series"
-            className="backToMixesBtn backToMixesBtn-desktop"
-          >
-            ⟵ Back to Mixes
-          </Link>
-        </div>
-        <div className="mixTextContent">
-          {/* Mobile back button at end of text */}
-          <Link
-            href="/mix-series"
-            className="backToMixesBtn backToMixesBtn-mobile"
-          >
-            ⟵ Back to Mixes
-          </Link>
-          <h2 style={!!mix.note ? { marginBottom: 0 } : {}}>{mix.name}</h2>
-          {!!mix.note && <p style={{ marginTop: 0 }}>{mix.note}</p>}
-          {/* Mobile image at end of text */}
-          <Image
-            src={mix.coverImage}
-            alt={`${mix.name} cover image`}
-            className="mixCoverImage mixCoverImage-mobile noDesktop"
-            width={500}
-            height={500}
-          />
+    <>
+      <div id="mixPage">
+        <div className="rowContainer">
+          <div className="leftColumn">
+            <Image
+              src={mix.coverImage}
+              alt={`${mix.name} cover image`}
+              className="mixCoverImage mixCoverImage-desktop"
+              width={500}
+              height={500}
+              onClick={() => {
+                console.log("open");
+                setOpenImage(true);
+              }}
+            />
+            <Link
+              href="/mix-series"
+              className="backToMixesBtn backToMixesBtn-desktop"
+            >
+              ⟵ Back to Mixes
+            </Link>
+          </div>
+          <div className="mixTextContent">
+            {/* Mobile back button at end of text */}
+            <Link
+              href="/mix-series"
+              className="backToMixesBtn backToMixesBtn-mobile"
+            >
+              ⟵ Back to Mixes
+            </Link>
+            <Link href={mix.soundcloudLink} target="_blank">
+              <h2 style={!!mix.note ? { marginBottom: 0 } : {}}>{mix.name}</h2>
+            </Link>
+            {!!mix.note && <p style={{ marginTop: 0 }}>{mix.note}</p>}
+            {/* Mobile image at end of text */}
+            <Image
+              src={mix.coverImage}
+              alt={`${mix.name} cover image`}
+              className="mixCoverImage mixCoverImage-mobile noDesktop"
+              width={500}
+              height={500}
+              onClick={() => {
+                console.log("open");
+                setOpenImage(true);
+              }}
+            />
 
-          {!!mix.dialog && (
-            <>
-              {/* <div className="introBox">
+            {!!mix.dialog && (
+              <>
+                {/* <div className="introBox">
                 <p style={{ marginBottom: "5px", fontStyle: "italic" }}>
                   Our Mix Series returns in a new form, a convergence of sounds,
                   stories and original graphics for a special edition of five
@@ -90,67 +97,81 @@ export default function Mix({ params }: eventPageParams) {
                 </p>
               </div> */}
 
-              <div style={{ whiteSpace: "pre-line" }}>{mix.description}</div>
+                <div style={{ whiteSpace: "pre-line", textAlign: "justify" }}>
+                  {mix.description}
+                </div>
 
-              {mix.dialog && (
-                <div
-                  className="dialogLabel"
-                  style={{
-                    borderTop: "1px solid red",
-                    marginTop: "30px",
-                    paddingTop: "30px",
-                  }}
-                >
-                  <span className="dialogSymbol">[</span>
-                  <span className="dialogChar"> d</span>
-                  <span className="dialogChar">i</span>
-                  <span className="dialogChar">a</span>
-                  <span className="dialogSymbol"> /</span>
-                  <span className="dialogChar"> l</span>
-                  <span className="dialogChar">o</span>
-                  <span className="dialogChar">g </span>
-                  <span className="dialogSymbol">]</span>
-                </div>
-              )}
-            </>
-          )}
-          {mix.dialog && (
-            <div id="dialogSection">
-              {mix.dialog.map((entry, index) => (
-                <div key={index} className="dialogEntry">
-                  <strong>{entry.talker}:</strong>{" "}
-                  <p style={{ whiteSpace: "pre-wrap" }}>
-                    {newLineOnDot(entry.message)}
-                  </p>
-                </div>
-              ))}
-            </div>
-          )}
+                {mix.dialog && (
+                  <div
+                    className="dialogLabel"
+                    style={{
+                      borderTop: "1px solid red",
+                      marginTop: "30px",
+                      paddingTop: "30px",
+                    }}
+                  >
+                    <span className="dialogSymbol">[</span>
+                    <span className="dialogChar"> d</span>
+                    <span className="dialogChar">i</span>
+                    <span className="dialogChar">a</span>
+                    <span className="dialogSymbol"> /</span>
+                    <span className="dialogChar"> l</span>
+                    <span className="dialogChar">o</span>
+                    <span className="dialogChar">g </span>
+                    <span className="dialogSymbol">]</span>
+                  </div>
+                )}
+              </>
+            )}
+            {mix.dialog && (
+              <div id="dialogSection">
+                {mix.dialog.map((entry, index) => (
+                  <div key={index} className="dialogEntry">
+                    <strong>{entry.talker}:</strong>{" "}
+                    <p style={{ whiteSpace: "pre-wrap" }}>
+                      {newLineOnDot(entry.message)}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div
+          style={{
+            position: "fixed",
+            bottom: 0,
+            left: 0,
+            zIndex: 100,
+            border: 0,
+            backgroundColor: "#880101ff",
+            width: "100%",
+            padding: "15px",
+          }}
+        >
+          <div>
+            <iframe
+              width="100%"
+              height="20"
+              scrolling="no"
+              frameBorder="no"
+              allow="autoplay"
+              src={mix.soundcloudEmbed}
+            />
+          </div>
         </div>
       </div>
 
-      <div
-        style={{
-          position: "fixed",
-          bottom: 0,
-          left: 0,
-          zIndex: 100,
-          border: 0,
-          backgroundColor: "black",
-          width: "100%",
-        }}
-      >
-        <div>
-          <iframe
-            width="100%"
-            height="20"
-            scrolling="no"
-            frameBorder="no"
-            allow="autoplay"
-            src={mix.soundcloudEmbed}
-          />
-        </div>
-      </div>
-    </div>
+      <ModalView isOpen={openImage} closeModal={() => setOpenImage(false)}>
+        <Image
+          src={mix.coverImage}
+          alt={`${mix.name} cover image`}
+          className="openImage"
+          width={1000}
+          height={1000}
+        />
+      </ModalView>
+    </>
   );
 }
--- src/components/ModalView.tsx
+++ src/components/ModalView.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import "./modalView.css";
 
 export default function ModalView({
-    children,
-    closeModal,
+  children,
+  closeModal,
+  isOpen,
 }: {
-    children: React.ReactNode;
-    closeModal: () => void;
+  children: React.ReactNode;
+  closeModal: () => void;
+  isOpen: boolean;
 }) {
-    return (
-        <div id="modalBackground">
-            <p id="close" onClick={closeModal}>
-                X
-            </p>
-            {children}
-        </div>
-    );
+  return (
+    <div
+      className={isOpen ? "isOpen" : "isClosed"}
+      id="modalBackground"
+      onClick={(e) => closeModal()}
+    >
+      <p id="close" onClick={closeModal}>
+        X
+      </p>
+      <div onClick={(e) => e.stopPropagation()}>{children}</div>
+    </div>
+  );
 }
